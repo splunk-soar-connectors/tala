bb9225b99898c10e591357ded950104c774e9f80
Alexandra Lomotan <alomotan@splunk.com>
2018-05-08 15:52:35 -0700

    PAPP-4532

---
+++ tala_consts.py
@@ -0,0 +1,11 @@
+# File: tala_consts.py
+# Copyright (c) 2018 Splunk Inc.
+#
+# SPLUNK CONFIDENTIAL â€“ Use or disclosure of this material in whole or in part
+# without a valid written license from Splunk Inc. is PROHIBITED.TALA_GET_PROJECT_SETTINGS_SUCC = "Successfully retrieved project settings"
+
+TALA_GET_SCAN_SETTINGS_SUCC = "Successfully retrieved scan settings"
+TALA_UPDATE_PROJECT_SUCC = "Successfully updated project"
+TALA_DELETE_PROJECT_SUCC = "Successfully deleted project"
+TALA_UPDATE_PROJECT_SETTINGS_SUCC = "Successfully updated project settings"
+TALA_ALREADY_DELETED_PROJECT_SUCC = "Project not found in Tala"
